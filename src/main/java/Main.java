b27d6be02f5bdbc9c910d23a520927f26897bad5
asanahmadli <asenahmedli@gmail.com>
2024-04-15 14:56:35 -0400

    I have add some changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,8 @@
 import org.apache.commons.io.IOUtils;
-import java.io.IOException;
+
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
 
@@ -9,9 +12,82 @@ public class Main {
         return result;
     }
 
+
+
+
     public static void main(String[] args) throws Exception{
         String output = (new Main()).readRawDataToString();
-        System.out.println(output);
+        //store result in the stringbuilder
+        StringBuilder sb = new StringBuilder();
+
+        //create arraylist store keys
+        List<String> keys= new ArrayList<>();
+        //create arraylist store values
+        List<String> values = new ArrayList<>();
+        //create delimeter
+        String delims = "[*;##%^]+";
+        //split string use delimeter
+        String[] splitString = output.split(delims);
+        //System.out.println(Arrays.toString(splitString));
+        int errors = 0;
+        List<String> updatedString = new ArrayList<>();
+        //store expiraion dates for split again
+        List<String> dates = new ArrayList<>();
+        for(int i = 0; i<splitString.length;i++){
+
+            String[] s = splitString[i].split(":");
+            if(s.length>1){
+                updatedString.add(splitString[i]);
+            }
+            else {
+                errors++;
+            }
+
+        }
+        //now I have updated new string
+        //create list contains only name and price
+        //System.out.println(errors);
+        List<String> namePrice = new ArrayList<>();
+        for(String s:updatedString){
+            if(s.contains("type")||s.contains("expiration")){
+
+            }
+            else {
+                namePrice.add(s);
+
+            }
+        }
+        //count products with their prices
+        int samePrice = 0;
+        int differentPrice = 0;
+        int countProduct = 0;
+        for(int b = 0; b<namePrice.size();b++){
+
+            String[] last = namePrice.get(b).split(":");
+
+               Pattern pattern = Pattern.compile("Milk",Pattern.CASE_INSENSITIVE);
+               Matcher matcher = pattern.matcher(last[1]);
+               if(matcher.find()){
+                   countProduct++;
+               }
+
+
+        }
+        System.out.println(countProduct);
+        System.out.println(namePrice);
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     }
 }
